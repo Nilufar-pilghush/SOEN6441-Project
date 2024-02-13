6e4429cf31d6a8e5c4e04aa63b6985ea9e7024a3
jeromekithinji <kithinjijerome@gmail.com>
2024-02-13 14:21:03 -0500

    Create and save game enviromentment methods added

--- Warzone/src/main/java/com/warzone/Service/GameMapDataHandler.java
+++ Warzone/src/main/java/com/warzone/Service/GameMapDataHandler.java
@@ -1,10 +1,19 @@
 package main.java.com.warzone.Service;
 
+import java.io.InputStream;
+import java.io.OutputStream;
+
 /**
  * Blueprint for managing game environment/map data.
  * Needs to handle game environment like creating, saving to specific files.
  */
 
 public interface GameMapDataHandler {
+
+    // create the game environment with the given file
+    void createGameEnvironment(InputStream p_InputStream);
+
+    // save current game environment in a file
+    void saveGameEnvironment(OutputStream p_OutputStream);
 
 }
