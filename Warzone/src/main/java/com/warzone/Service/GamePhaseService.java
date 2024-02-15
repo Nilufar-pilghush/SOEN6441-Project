66bb46c0e876f5a09c6617ab9bb105cebab5d1d2
jeromekithinji <kithinjijerome@gmail.com>
2024-02-14 23:19:16 -0500

    Renamed interface and method

--- Warzone/src/main/java/com/warzone/Service/GamePhaseService.java
+++ Warzone/src/main/java/com/warzone/Service/GamePhaseService.java
@@ -2,6 +2,6 @@ package main.java.com.warzone.Service;
 
 import main.java.com.warzone.Entities.GamePhase;
 
-public interface PhaseService {
-    GamePhase handlePhase(GamePhase p_CurrPhase);
+public interface GamePhaseService {
+    GamePhase handleGamePhase(GamePhase p_CurrPhase);
 }
