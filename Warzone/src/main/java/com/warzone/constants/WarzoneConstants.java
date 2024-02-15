4fceb35fbbff4f9df8484ba0960ed13647a824e9
Ali Sayed Salehi <a.s.salehi.95@gmail.com>
2024-02-14 20:34:58 -0500

    add cmdline tokenizer util

---
+++ Warzone/src/main/java/com/warzone/constants/WarzoneConstants.java
@@ -0,0 +1,21 @@
+package main.java.com.warzone.constants;
+
+/**
+ * Class containing all the constants referenced in the code.
+ *
+ * @author Alli sayed Salehi
+ * @version 1.0.0
+ */
+public class WarzoneConstants {
+
+    /**
+     * Constant to split on hyphen
+     */
+    public static final String HYPHEN = "-";
+
+    /**
+     * Constant that matches any length of whitespace
+     */
+    public static final String SPACE_REGEX = "\\s*";
+
+}
