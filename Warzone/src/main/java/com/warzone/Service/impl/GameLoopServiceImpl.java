417c6fce8939db696467fe4b227c96ae26f30bd8
jeromekithinji <kithinjijerome@gmail.com>
2024-02-15 14:21:51 -0500

    Added the actions to user input commands

--- Warzone/src/main/java/com/warzone/Service/impl/GameLoopServiceImpl.java
+++ Warzone/src/main/java/com/warzone/Service/impl/GameLoopServiceImpl.java
@@ -1,13 +1,11 @@
 package main.java.com.warzone.Service.impl;
 
+import main.java.com.warzone.Entities.Country;
 import main.java.com.warzone.Entities.GamePhase;
 import main.java.com.warzone.Entities.GameSession;
 import main.java.com.warzone.Service.GamePhaseService;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
 
 public class GameLoopServiceImpl implements GamePhaseService {
 
@@ -54,21 +52,44 @@ public class GameLoopServiceImpl implements GamePhaseService {
                 String l_PrimaryCommand = l_UserInputTokens.get(0).toLowerCase();
                 switch (l_PrimaryCommand) {
                     case "showmap" -> {
-                        System.out.println("Map display");
-                        // need to add show map method
+                        showMap();
                     }
                     case "help" -> {
-                        System.out.println("Help display");
+                        // Will display help text
                     }
                     case "exit" -> {
                         System.out.println("Exit game");
+                        return GamePhase.EXIT;
                     }
                     default -> {
-                        System.out.println("Continue with game");
+                        d_GameSession.setCurrGamePhase(l_NextPhase);
                     }
                 }
-            } catch (IndexOutOfBoundsException e) {
-                System.out.println("No input given");
+            }
+            // Proceed with game if no input is given
+            catch (IndexOutOfBoundsException e) {
+                d_GameSession.setCurrGamePhase(l_NextPhase);
+            }
+        }
+    }
+
+    private void showMap() {
+        System.out.println("Showing map");
+        System.out.println("Continents:");
+        for (String l_ContinentName : d_GameSession.getContinentsInSession().keySet()) {
+            System.out.println(l_ContinentName);
+            System.out.println("Countries:");
+
+            Iterator<Map.Entry<String, Country>> l_CountriesInContinent = d_GameSession.getContinentsInSession().get(l_ContinentName).getD_Countries().entrySet().iterator();
+            while (l_CountriesInContinent.hasNext()) {
+                Map.Entry<String, Country> l_Country = l_CountriesInContinent.next();
+                System.out.println(l_Country.getKey());
+                System.out.println("Armies: " + l_Country.getValue().get_NumberOfArmies());
+                System.out.println("Player owner: " + l_Country.getValue().get_Owner());
+                System.out.println("Adjacent countries:");
+                for (String l_AdjacentCountry : l_Country.getValue().getD_AdjacentCountries().values()) {
+                    System.out.print(l_AdjacentCountry + ", ");
+                }
             }
         }
     }
