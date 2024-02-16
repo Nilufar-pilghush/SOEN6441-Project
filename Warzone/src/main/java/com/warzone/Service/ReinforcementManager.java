d28ca0d3f55fe2cfba9b4065dd9e70d0ab612920
Fate <fatemechaji76@gmail.com>
2024-02-16 01:36:13 -0500

    Ensured naming conventions

--- Warzone/src/main/java/com/warzone/Service/ReinforcementManager.java
+++ Warzone/src/main/java/com/warzone/Service/ReinforcementManager.java
@@ -8,16 +8,16 @@ import java.util.Iterator;
 import java.util.Map;
 
 public class ReinforcementManager implements GamePhaseService {
-    private GameSession gameSession;
-    private int minimumReinforcementArmies;
+    private GameSession d_gameSession;
+    private int d_minimumReinforcementArmies;
 
     public ReinforcementManager() {
-        gameSession = GameSession.getInstance();
-        minimumReinforcementArmies = 3;
+        d_gameSession = GameSession.getInstance();
+        d_minimumReinforcementArmies = 3;
     }
 
     @Override
-    public GamePhase handleGamePhase(GamePhase currentSegment) {
+    public GamePhase handleGamePhase(GamePhase p_currentSegment) {
         System.out.println("Handling reinforcement phase");
         checkWinningCondition();
         reinforcePlayers();
@@ -26,45 +26,44 @@ public class ReinforcementManager implements GamePhaseService {
     }
 
     private void checkWinningCondition() {
-        for (Player player : gameSession.getPlayers().values()) {
-            if (player.getOwnedCountries().size() == gameSession.getCountriesInSession().size()) {
-                System.out.println(player.getName() + " has won the game!");
+        for (Player l_player : d_gameSession.getPlayers().values()) {
+            if (l_player.getOwnedCountries().size() == d_gameSession.getCountriesInSession().size()) {
+                System.out.println(l_player.getName() + " has won the game!");
                 System.exit(0);
             }
         }
     }
 
     private void reinforcePlayers() {
-        for (Player player : gameSession.getPlayers().values()) {
-            player.addArmies(Math.max(Math.floorDiv(player.getOwnedCountries().size(), 3), minimumReinforcementArmies));
+        for (Player l_player : d_gameSession.getPlayers().values()) {
+            l_player.addArmies(Math.max(Math.floorDiv(l_player.getOwnedCountries().size(), 3), d_minimumReinforcementArmies));
         }
     }
 
     private void updateContinentOwnershipAndReinforcements() {
-        for (Continent continent : gameSession.getContinentsInSession().values()) {
-            String continentOwner = continent.get_Owner();
-            if (continentOwner != null) {
-                Player continentOwnerPlayer = gameSession.getPlayers().get(continentOwner);
-                continentOwnerPlayer.addArmies(continent.get_ControlValue());
+        for (Continent l_continent : d_gameSession.getContinentsInSession().values()) {
+            String l_continentOwner = l_continent.get_Owner();
+            if (l_continentOwner != null) {
+                Player l_continentOwnerPlayer = d_gameSession.getPlayers().get(l_continentOwner);
+                l_continentOwnerPlayer.addArmies(l_continent.get_ControlValue());
             } else {
-                boolean isContinentOwned = true;
-                String continentOwnerPlayerName = null;
-                for (Country country : continent.getD_Countries().values()) {
-                    String countryOwner = country.get_Owner();
-                    if (continentOwnerPlayerName == null && countryOwner != null) {
-                        continentOwnerPlayerName = countryOwner;
-                    } else if (countryOwner == null || !countryOwner.equals(continentOwnerPlayerName)) {
-                        isContinentOwned = false;
+                boolean l_isContinentOwned = true;
+                String l_continentOwnerPlayerName = null;
+                for (Country l_country : l_continent.getD_Countries().values()) {
+                    String l_countryOwner = l_country.get_Owner();
+                    if (l_continentOwnerPlayerName == null && l_countryOwner != null) {
+                        l_continentOwnerPlayerName = l_countryOwner;
+                    } else if (l_countryOwner == null || !l_countryOwner.equals(l_continentOwnerPlayerName)) {
+                        l_isContinentOwned = false;
                         break;
                     }
                 }
-                if (isContinentOwned) {
-                    Player continentOwnerPlayer = gameSession.getPlayers().get(continentOwnerPlayerName);
-                    continent.set_Owner(continentOwnerPlayerName);
-                    continentOwnerPlayer.addArmies(continent.get_ControlValue());
+                if (l_isContinentOwned) {
+                    Player l_continentOwnerPlayer = d_gameSession.getPlayers().get(l_continentOwnerPlayerName);
+                    l_continent.set_Owner(l_continentOwnerPlayerName);
+                    l_continentOwnerPlayer.addArmies(l_continent.get_ControlValue());
                 }
             }
         }
     }
 }
-
