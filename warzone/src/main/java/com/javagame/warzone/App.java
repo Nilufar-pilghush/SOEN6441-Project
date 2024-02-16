2094dd48c0203934b5e13ebf9b82fe7d0ef5d840
Ali Sayed Salehi <a.s.salehi.95@gmail.com>
2024-02-15 20:22:03 -0500

    create maven project

---
+++ warzone/src/main/java/com/javagame/warzone/App.java
@@ -0,0 +1,13 @@
+package com.javagame.warzone;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
