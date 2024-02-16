889e21a2e4f07d32a19ef1bdd3da905c16012ab9
Fate <fatemechaji76@gmail.com>
2024-02-16 01:22:45 -0500

    Implemented required methods

--- Warzone/src/main/java/com/warzone/Service/ReinforcementManager.java
+++ Warzone/src/main/java/com/warzone/Service/ReinforcementManager.java
@@ -24,4 +24,47 @@ public class ReinforcementManager implements GamePhaseService {
         updateContinentOwnershipAndReinforcements();
         return GamePhase.MAIN_GAME_LOOP;
     }
+
+    private void checkWinningCondition() {
+        for (Player player : gameSession.getPlayers().values()) {
+            if (player.getOwnedCountries().size() == gameSession.getCountriesInSession().size()) {
+                System.out.println(player.getName() + " has won the game!");
+                System.exit(0);
+            }
+        }
+    }
+
+    private void reinforcePlayers() {
+        for (Player player : gameSession.getPlayers().values()) {
+            player.addArmies(Math.max(Math.floorDiv(player.getOwnedCountries().size(), 3), minimumReinforcementArmies));
+        }
+    }
+
+    private void updateContinentOwnershipAndReinforcements() {
+        for (Continent continent : gameSession.getContinentsInSession().values()) {
+            String continentOwner = continent.get_Owner();
+            if (continentOwner != null) {
+                Player continentOwnerPlayer = gameSession.getPlayers().get(continentOwner);
+                continentOwnerPlayer.addArmies(continent.get_ControlValue());
+            } else {
+                boolean isContinentOwned = true;
+                String continentOwnerPlayerName = null;
+                for (Country country : continent.getD_Countries().values()) {
+                    String countryOwner = country.get_Owner();
+                    if (continentOwnerPlayerName == null && countryOwner != null) {
+                        continentOwnerPlayerName = countryOwner;
+                    } else if (countryOwner == null || !countryOwner.equals(continentOwnerPlayerName)) {
+                        isContinentOwned = false;
+                        break;
+                    }
+                }
+                if (isContinentOwned) {
+                    Player continentOwnerPlayer = gameSession.getPlayers().get(continentOwnerPlayerName);
+                    continent.set_Owner(continentOwnerPlayerName);
+                    continentOwnerPlayer.addArmies(continent.get_ControlValue());
+                }
+            }
+        }
+    }
 }
+
