2b234a6a59bccca0e5b1e1b0b707e035a96a8883
nilufar_pilghush <140025053+FM-CONU@users.noreply.github.com>
2024-02-18 16:57:49 -0500

    Refactored PhaseTest

--- Warzone/src/test/java/com/warzone/entities/GamePhaseTest.java
+++ Warzone/src/test/java/com/warzone/entities/GamePhaseTest.java
@@ -1,65 +1,79 @@
 package test.java.com.warzone.entities;
 import main.java.warzone.entities.GamePhase;
 import main.java.warzone.services.GamePhaseService;
-import main.java.warzone.services.impl.*;
+import main.java.warzone.services.impl.MapEditorServiceImpl;
+import main.java.warzone.services.impl.OrderIssuanceServiceImpl;
+import main.java.warzone.services.impl.ReinforcementServiceImpl;
+import main.java.warzone.services.impl.StartupPhaseServiceImpl;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
 
-/**
- * Test class
- *
- * @author Niloufar Pilgush
- * @author Nasrin Maarefi
- * @author Jerome Kithinji
- * @author Ali sayed Salehi
- * @author Fatemeh Chaji
- * @version 1.0.0
- */
-public class GamePhaseTest {
     /**
+     * JUnit test cases for the game segment class.
+     * Test case to verify get scene editor segment.
+     * Test case to verify get startup segment.
+     * Test case to verify get reinforcement segment.
+     * Test case to verify get issue orders segment.
+     * Test case to verify get exit segment.
+     *
+     * @author Niloufar Pilgush
+     * @author Nasrin Maarefi
+     * @author Jerome Kithinji
+     * @author Ali sayed Salehi
+     * @author Fatemeh Chaji
+     * @version 1.0.0
      */
-    @Test
-    public void getWarzonePhaseMapEditorTest() {
-        GamePhase phase = GamePhase.MAP_EDITOR;
-        GamePhaseService service = phase.getWarzonePhase(phase);
-        assertTrue(service instanceof MapEditorServiceImpl);
-    }
+    public class GamePhaseTest {
 
-    /**
-     */
-    @Test
-    public void getWarzonePhaseStartUpTest() {
-        GamePhase segment = GamePhase.START_UP;
-        GamePhaseService service = segment.getWarzonePhase(segment);
-        assertTrue(service instanceof StartupPhaseServiceImpl);
-    }
+        /**
+         * Test case to verify scene editor segment.
+         */
+        @Test
+        public void getWarzoneSegmentSceneEditorTest() {
+            GamePhase phase = GamePhase.MAP_EDITOR;
+            GamePhaseService service = phase.getWarzonePhase(phase);
+            assertTrue(service instanceof MapEditorServiceImpl);
+        }
 
-    /**
-     */
-    @Test
-    public void getWarzonePhaseReinforcementTest() {
-        GamePhase phase = GamePhase.REINFORCEMENT;
-        GamePhaseService service = phase.getWarzonePhase(phase);
-        assertTrue(service instanceof ReinforcementServiceImpl);
-    }
+        /**
+         * Test case to verify startup segment.
+         */
+        @Test
+        public void getWarzoneSegmentStartUpTest() {
+            GamePhase phase = GamePhase.START_UP;
+            GamePhaseService service = phase.getWarzonePhase(phase);
+            assertTrue(service instanceof StartupPhaseServiceImpl);
+        }
 
-    /**
-     */
-    @Test
-    public void getWarzonePhaseIssueOrdersTest() {
-        GamePhase phase = GamePhase.ISSUE_ORDERS;
-        GamePhaseService service = phase.getWarzonePhase(phase);
-        assertTrue(service instanceof OrderIssuanceServiceImpl);
-    }
+        /**
+         * Test case to verify get reinforcement segment
+         */
+        @Test
+        public void getWarzoneSegmentReinforcementTest() {
+            GamePhase phase = GamePhase.REINFORCEMENT;
+            GamePhaseService service = phase.getWarzonePhase(phase);
+            assertTrue(service instanceof ReinforcementServiceImpl);
+        }
 
-    /**
-     */
-    @Test
-    public void getWarzoneSegmentExitTest() {
-        GamePhase phase = GamePhase.EXIT;
-        GamePhaseService service = phase.getWarzonePhase(phase);
-        assertNull(service);
+        /**
+         * Test case to verify get issue order segment
+         */
+        @Test
+        public void getWarzoneSegmentIssueOrdersTest() {
+            GamePhase phase = GamePhase.ISSUE_ORDERS;
+            GamePhaseService service = phase.getWarzonePhase(phase);
+            assertTrue(service instanceof OrderIssuanceServiceImpl);
+        }
+
+        /**
+         * Test case to verify exit segment.
+         */
+        @Test
+        public void getWarzoneSegmentExitTest() {
+            GamePhase phase = GamePhase.EXIT;
+            GamePhaseService service = phase.getWarzonePhase(phase);
+            assertNull(service);
+        }
     }
-}
