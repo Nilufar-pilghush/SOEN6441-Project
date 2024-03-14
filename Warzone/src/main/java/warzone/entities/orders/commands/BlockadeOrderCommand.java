e29eef5a76c980f5cc9a71037abb3fecb4769907
Fate <fatemechaji76@gmail.com>
2024-03-14 19:18:39 -0400

    Added BlockadeOrderCommand

---
+++ Warzone/src/main/java/warzone/entities/orders/commands/BlockadeOrderCommand.java
@@ -0,0 +1,14 @@
+package main.java.warzone.entities.orders.commands;
+
+import main.java.warzone.constants.WarzoneConstants;
+import main.java.warzone.entities.Country;
+import main.java.warzone.entities.GameSession;
+import main.java.warzone.entities.orders.Order;
+import main.java.warzone.entities.orders.OrderDetails;
+import main.java.warzone.utils.logging.impl.LogEntryBuffer;
+
+public class BlockadeOrderCommand extends Order {
+
+
+
+}
