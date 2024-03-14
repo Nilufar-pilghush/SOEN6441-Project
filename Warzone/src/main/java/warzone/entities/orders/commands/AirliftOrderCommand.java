b8c41eb3602512fcde7e310f78c7abf110480b96
Fate <fatemechaji76@gmail.com>
2024-03-14 19:39:32 -0400

    Overrided the execute method and added constructor

--- Warzone/src/main/java/warzone/entities/orders/commands/AirliftOrderCommand.java
+++ Warzone/src/main/java/warzone/entities/orders/commands/AirliftOrderCommand.java
@@ -9,6 +9,46 @@ import main.java.warzone.utils.logging.impl.LogEntryBuffer;
 
 public class AirliftOrderCommand extends Order {
 
+    private LogEntryBuffer d_LogEntryBuffer;
 
+    public AirliftOrderCommand(String p_PlayerName, String p_SourceCountry, String p_TargetCountry, int p_NumberOfArmies) {
+        super(p_PlayerName, p_SourceCountry, p_TargetCountry, p_NumberOfArmies);
+        this.d_LogEntryBuffer = LogEntryBuffer.getInstance();
+        this.setOrderType(WarzoneConstants.AIRLIFT);
+    }
+
+    @Override
+    public void execute(GameSession p_GameSession) {
+        OrderDetails l_OrderDetails = this.getOrderDetails();
+        d_LogEntryBuffer.logData("Executing airlift order for " + l_OrderDetails.getPlayerName() + " from " + l_OrderDetails.getSourceCountry() + " to " + l_OrderDetails.getTargetCountry() + " with " + l_OrderDetails.getNumberOfArmies() + " armies");
+        Country l_SourceCountry = p_GameSession.getCountriesOfSession().get(l_OrderDetails.getSourceCountry());
+        Country l_TargetCountry = p_GameSession.getCountriesOfSession().get(l_OrderDetails.getTargetCountry());
+        // If not owned by the player, then subtract armies & update owner
+        int l_RemainingAttackers = this.getRemainingAttackers(l_OrderDetails.getNumberOfArmies(), l_TargetCountry.getNumberOfArmies());
+        int l_RemainingDefenders = this.getRemainingDefenders(l_OrderDetails.getNumberOfArmies(), l_TargetCountry.getNumberOfArmies());
+        if (l_TargetCountry.getOwner() == null || !l_TargetCountry.getOwner().equals(l_OrderDetails.getPlayerName())) {
+            if (l_RemainingDefenders == 0 && l_RemainingAttackers > 0) {
+                d_LogEntryBuffer.logData("Capture successful");
+                this.setCaptureFlag(true);
+                if (l_TargetCountry.getOwner() != null) {
+                    Player l_OldOwner = p_GameSession.getPlayers().get(l_TargetCountry.getOwner());
+                    l_OldOwner.removeOwnedCountry(l_OrderDetails.getTargetCountry());
+                }
+                l_TargetCountry.setOwner(l_OrderDetails.getPlayerName());
+                Player l_NewOwner = p_GameSession.getPlayers().get(l_OrderDetails.getPlayerName());
+                l_NewOwner.addOwnedCountry(l_TargetCountry.getName());
+                l_TargetCountry.setNumberOfArmies(l_RemainingAttackers);
+            } else {
+                d_LogEntryBuffer.logData("Capture failed");
+                l_TargetCountry.setNumberOfArmies(l_RemainingDefenders);
+            }
+        } else {
+            d_LogEntryBuffer.logData("Adding armies to owned country");
+            l_TargetCountry.addArmies(l_OrderDetails.getNumberOfArmies());
+        }
+        // Reduce source country armies
+        l_SourceCountry.subtractArmies(l_OrderDetails.getNumberOfArmies());
+        this.runPostExecute(p_GameSession);
+    }
 
 }
