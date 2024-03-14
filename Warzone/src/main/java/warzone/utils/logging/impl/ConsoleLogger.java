f357656cf88b334248c88e5a4c70d311e4863ff8
Ali Sayed Salehi <a.s.salehi.95@gmail.com>
2024-03-13 21:06:22 -0400

    fix typo

--- Warzone/src/main/java/warzone/utils/logging/impl/ConsoleLogger.java
+++ Warzone/src/main/java/warzone/utils/logging/impl/ConsoleLogger.java
@@ -6,12 +6,12 @@ import main.java.warzone.utils.logging.Observer;
  * A realization of {@link Observer} to update the messages in the
  * console of the warzone game
  *
- * @author Snehil Sharma
- * @author Jatin
- * @author Kenish Rajeshbhai Halani
- * @author Udhisha
- * @author Aazam Arvind
- * @since 2.0.0
+ * @author Niloufar Pilgush
+ * @author Nasrin Maarefi
+ * @author Jerome Kithinji
+ * @author Ali sayed Salehi
+ * @author Fatemeh Chaji
+ * @since 1.0.0
  */
 public class ConsoleLogger implements Observer {
 
