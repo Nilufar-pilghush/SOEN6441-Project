299f4f9f1c865d6ec6c7d6c9c0a37ba67efcd54c
Fate <fatemechaji76@gmail.com>
2024-04-03 11:55:59 -0400

    Added proper methods for saving and loading.

--- Warzone/src/main/java/warzone/utils/GameProgressTracker.java
+++ Warzone/src/main/java/warzone/utils/GameProgressTracker.java
@@ -2,8 +2,9 @@
 package main.java.warzone.utils;
 
 import main.java.warzone.entities.GameSession;
-import main.java.warzone.entities.Continent;
-import main.java.warzone.entities.Country;
+import main.java.warzone.constants.WarzoneConstants;
+import main.java.warzone.exceptions.WarzoneValidationException;
+import main.java.warzone.utils.logging.impl.LogEntryBuffer;
 
 import java.util.*;
 
@@ -16,4 +17,59 @@ public class GameProgressTracker {
         d_CurrGameSession = GameSession.getInstance();
         d_LogEntryBuffer = LogEntryBuffer.getInstance();
     }
+
+    public boolean saveGameProgress(String p_FileName) {
+        try {
+            FileOutputStream l_FileOutputStream = new FileOutputStream(WarzoneConstants.SAVED_GAMES + WarzoneConstants.FORWARD_SLASH + p_FileName);
+            ObjectOutputStream l_ObjectOutputStream = new ObjectOutputStream(l_FileOutputStream);
+            l_ObjectOutputStream.writeObject(d_CurrGameSession);
+            l_ObjectOutputStream.flush();
+            l_FileOutputStream.close();
+            d_CurrGameSession.clearPreviousSession();
+            d_LogEntryBuffer.logData("The game has been successfully saved to file /GameSessions/" + p_FileName);
+            return true;
+        } catch (Exception p_Exception) {
+            d_LogEntryBuffer.logData(p_Exception.toString());
+            return false;
+        }
+    }
+
+    public void listSavedGameFiles() {
+        d_LogEntryBuffer.logData("..............................................");
+        d_LogEntryBuffer.logData("\t\t Warzone - Load Menu");
+        d_LogEntryBuffer.logData("..............................................");
+        d_LogEntryBuffer.logData("\t\t\t Load Game");
+        d_LogEntryBuffer.logData("\t.............................\n");
+        File[] l_Files = new File(WarzoneConstants.SAVED_GAMES + WarzoneConstants.FORWARD_SLASH).listFiles();
+        if (l_Files.length > 0) {
+            for (File l_SavedGame : l_Files) {
+                d_LogEntryBuffer.logData("\t\t\t" + l_SavedGame.getName());
+            }
+        } else {
+            d_LogEntryBuffer.logData("\t\t " + "no saved games found");
+        }
+        d_LogEntryBuffer.logData("");
+        d_LogEntryBuffer.logData("\t.......................");
+        d_LogEntryBuffer.logData("\t use file name to load");
+        d_LogEntryBuffer.logData("...............................");
+        d_LogEntryBuffer.logData("example command: loadgame filename");
+    }
+
+    public GameSegment loadGameProgress(String p_FileName) {
+        try {
+            FileInputStream l_FileInputStream = new FileInputStream(WarzoneConstants.SAVED_GAMES + WarzoneConstants.FORWARD_SLASH + p_FileName);
+            ObjectInputStream l_Os = new ObjectInputStream(l_FileInputStream);
+            GameSession l_LoadedGameMap = (GameSession) l_Os.readObject();
+            d_LogEntryBuffer.logData("The game is successfully loaded and will continue from where it last stopped.");
+            l_Os.close();
+            return GameSession.getInstance().makeGameFromLoaded(l_LoadedGameMap);
+        } catch (IOException | ClassNotFoundException e) {
+            d_LogEntryBuffer.logData("The file could not be loaded.");
+            return GameSegment.START_UP;
+        } catch (WarzoneValidationException e) {
+            d_LogEntryBuffer.logData("Failed to create game from saved");
+            return GameSegment.START_UP;
+        }
+    }
+
 }
