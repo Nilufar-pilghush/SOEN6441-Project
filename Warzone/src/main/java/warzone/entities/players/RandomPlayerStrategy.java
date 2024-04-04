bf960c8dd47b61a091f6ec304b75eae92be7bc3f
Fate <fatemechaji76@gmail.com>
2024-04-04 02:42:15 -0400

    Added helper functions for overriding the required methods.

--- Warzone/src/main/java/warzone/entities/players/RandomPlayerStrategy.java
+++ Warzone/src/main/java/warzone/entities/players/RandomPlayerStrategy.java
@@ -1,3 +1,32 @@
 package main.java.warzone.entities.players;
 
-public class RandomPlayerStrategy implements PlayerStrategy, Serializable {}
+import main.java.warzone.entities.GameSession;
+import main.java.warzone.entities.Country;
+import main.java.warzone.entities.Player;
+import main.java.constants.WarzoneConstants;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.Serializable;
+import java.util.List;
+import java.util.ArrayList;
+public class RandomPlayerStrategy implements PlayerStrategy, Serializable {
+
+    @Override
+    public void issuePlayerOrder(Player p_Player, GameSession p_GameSession) {}
+
+
+    @Override
+    public String getStrategyNameString() {
+        return WarzoneConstants.RANDOM;
+    }
+
+    private Country getRandomOwnedCountry(Player p_Player, GameSession p_GameSession) {
+        List<Country> ownedCountries = new ArrayList<>();
+        for (String l_CountryName : p_Player.getOwnedCountries()) {
+            ownedCountries.add(p_GameSession.getCountriesOfSession().get(l_CountryName));
+        }
+        return ownedCountries.get((int) (Math.random() * ownedCountries.size()));
+    }
+
+}
